45b182cd5de0393006e129811a7bf15da33a84ae
Dawid Kwidzinski <kwidzinskidawid@gmail.com>
2016-04-05 01:05:25 +0200

    jbehave working

---
+++ messenger 2/src/main/resources/com/example/mockdemo/app/messenger_test.story
@@ -0,0 +1,14 @@
+Given a messenger where 
+	valid server is inf.ug.edu.pl and invalid server is inf.ug.edu.eu
+	valid message is somelongmessage and invalid message is ab
+
+When server is valid and message is valid
+Then checking connection should return 0
+Then sending message should return code status 0
+
+When server is invalid and message is valid
+Then checking connection should return 1
+Then sending message should return code status 1
+
+When server is valid and message is invalid
+Then sending message should return code status 2
--- messenger 2/src/test/java/com/example/mockdemo/app/MessengerSteps.java
+++ messenger 2/src/test/java/com/example/mockdemo/app/MessengerSteps.java
@@ -1,34 +1,89 @@
 package com.example.mockdemo.app;
 
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-import com.example.mockdemo.messenger.MessageServiceSimpleImpl;
+import com.example.mockdemo.messenger.ConnectionStatus;
+import com.example.mockdemo.messenger.MalformedRecipientException;
+import com.example.mockdemo.messenger.MessageService;
+import com.example.mockdemo.messenger.SendingStatus;
 
 public class MessengerSteps {
 	
 	private Messenger messenger;
+	private MessageService msMock;
 	
-	@Given("a messenger")
-	public void calulatorSetup(){
-		messenger = new Messenger(new MessageServiceSimpleImpl());
+	private static String VALID_SERVER;
+	private static String INVALID_SERVER;
+
+	private static String VALID_MESSAGE;
+	private static String INVALID_MESSAGE;
+	
+	private String currentServer;
+	private String currentMsg;
+	
+	@Given("a messenger where valid server is $vs and invalid server is $is valid message is $vm and invalid message is $im")
+	public void calulatorSetup(String vs, String is, String vm, String im) throws MalformedRecipientException{
+		msMock = mock(MessageService.class);
+		messenger = new Messenger(msMock);
+		
+		VALID_SERVER = vs;
+		INVALID_SERVER = is;
+		VALID_MESSAGE = vm;
+		INVALID_MESSAGE = im;
+		
+		when(msMock.send(VALID_SERVER, VALID_MESSAGE)).thenReturn(
+				SendingStatus.SENT);
+		when(msMock.send(VALID_SERVER, INVALID_MESSAGE)).thenThrow(
+				new MalformedRecipientException());
+		when(msMock.send(INVALID_SERVER, VALID_MESSAGE)).thenReturn(
+				SendingStatus.SENDING_ERROR);
+		when(msMock.send(INVALID_SERVER, INVALID_MESSAGE)).thenReturn(
+				SendingStatus.SENDING_ERROR);
+		
+
+		when(msMock.checkConnection(INVALID_SERVER)).thenReturn(
+				ConnectionStatus.FAILURE);
+		when(msMock.checkConnection(VALID_SERVER)).thenReturn(
+				ConnectionStatus.SUCCESS);
+	}	
+	
+	
+	@When("server is $server and message is $msg")
+	public void setArguments(String server, String msg){
+		this.currentServer = server.equals("valid") ? VALID_SERVER : INVALID_SERVER;
+		this.currentMsg = msg.equals("valid") ? VALID_MESSAGE : INVALID_MESSAGE;
 	}
 	
-	@When("set arguments to $a and $b")
-	public void setArguments(int a, int b){
+    @Then("sending message should return code status $result")
+	public void checkSending(int result) throws MalformedRecipientException{
 
-	}
-	
-    @Then("adding should return $result")
-	public void shouldAdd(int result){
-
+			if (result == 0 ) {
+				assertThat(messenger.sendMessage(currentServer, currentMsg),
+						either(equalTo(0)).or(equalTo(1)));
+			} else {
+				assertEquals(result, messenger.sendMessage(currentServer, currentMsg));
+			}
+			
+			verify(msMock, atLeastOnce()).send(currentServer, currentMsg);
 	}
     
-    @Then("subtracting should return $result")
-  	public void shouldSubstract(int result){
+    @Then("checking connection should return $result")
+    public void checkChecking(int result) {
+    	
+		assertEquals(result, messenger.testConnection(currentServer));
+		
+		verify(msMock, atLeastOnce()).checkConnection(currentServer);
+    }
 
-  	}
 }
